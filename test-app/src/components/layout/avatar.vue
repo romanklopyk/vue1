d19e2704289810ddff45f6aae222c17efb9fa957
romanklopyk <86305348+romanklopyk@users.noreply.github.com>
2021-08-17 19:44:26 +0300

    rrr

---
+++ test-app/src/components/layout/avatar.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  props: [],
+};
+</script>
+
+<style lang="scss">
+.avatar {
+  width: 96px;
+  height: 96px;
+  border-radius: 50%;
+  background: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ test-app/src/components/layout/header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <div>
+      <label for="">Ім'я</label>
+
+      <input type="text" v-model="user.first_name" />
+    </div>
+
+    <div>
+      <label for="">Прізвище</label>
+
+      <input type="text" v-model="user.last_name" />
+    </div>
+
+    <div class="avatar">{{ initials }}</div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        first_name: "",
+        last_name: "",
+      },
+    };
+  },
+  computed: {
+    initials() {
+      var letters = "AB";
+      if (
+        this.user.first_name &&
+        !this.user.last_name &&
+        this.user.first_name.length >= 2
+      ) {
+        letters = this.user.first_name[0] + this.user.first_name[1];
+      }
+      if (this.user.first_name && this.user.last_name) {
+        letters = this.user.first_name[0] + this.user.last_name[0];
+      }
+      return letters.toUpperCase();
+    },
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss">
+</style>
